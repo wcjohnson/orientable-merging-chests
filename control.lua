83c5f854af5028e5a182ee839c17739b957d0fb5
William C. Johnson <wcjohnson@oigroup.net>
2026-06-07 18:30:25 -0700

    feat: Remove defunct blueprint rotation

--- control.lua
+++ control.lua
@@ -2,5 +2,4 @@ require('init')
 require('control_init')
 
 require('scripts.event_handlers.merge_chest')
-require('scripts.event_handlers.rotate_blueprint')
 require('scripts.event_handlers.split_chest')
